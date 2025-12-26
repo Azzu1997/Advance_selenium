cfa66c64f0d1e4afdfd39fdead613fb90b86ec20
shaik <shaik@Azmat>
2025-12-26 07:08:00 +0530

    New code is ready

---
+++ src/test/java/Contacts/DummyProject.java
@@ -0,0 +1,11 @@
+package Contacts;
+
+public class DummyProject
+{
+public static void main(String[] args)
+{
+	System.out.println("-----New Samaple");
+	System.out.println("-----dummy Code");
+
+}
+}
